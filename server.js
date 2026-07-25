aa679bb49b1c1ffc30c8d76ba2d6e017cf919ab2
safderali1122003-cmyk <safder.ali1122003@gmail.com>
2026-07-25 19:20:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -24,3 +24,4 @@ app.post('/api/withdraw', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
